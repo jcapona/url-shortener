2148136e5bb6632c601c210cec77a727dada73c3
Jorge Capona <jcapona@gmail.com>
2016-01-12 20:09:37 -0300

    Connects to db for each request

--- index.js
+++ index.js
@@ -6,62 +6,63 @@ var mongo = require('mongodb').MongoClient;
 var express = require('express');
 var app = express();
 
-mongo.connect(MONGOLAB_URI, function(err, db) {
-  if(err)
-    return console.dir(err);
-    
-  console.log("Connected to Mongolab DB");
-    
-  app.set('port', (process.env.PORT || 5000));
-  app.set('view engine', 'jade');
+// ROUTES 
+app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'jade');
 
-  // Routes
-  app.get("/", function(request, response) {
-    response.render("index");
-  });
-
-  app.get("/*", function(request, response) {
-    var url = request.params[0];
-    var short_url = "";
-    var json = JSON.stringify({"error":"URL invalid"});
-
-    if(url.match(DET) === null)
-      response.send(json);  
-    
-    shorten(db,url,function(err,resp){
-      if(err)
-        console.error("Error: "+err);
-      else
-        json = JSON.stringify({"original_url":url,"short_url":resp});
-        
-      response.send(json);            
-    });
-    
-  });
-
-
-  function shorten(db, url, callback)
-  {
-    var col = db.collection('shorten');
-    col.find({
-      url: url
-    }).toArray(function(err, documents) {
-      console.log("finished looking for url...");
-      var resp = "";
-      if(err)
-        callback(err,null);
-      else if(documents.length)
-        resp = documents[0].short;
-      else
-      {
-        //TODO: Create new short url
-      }
-
-      db.close();
-      callback(null,resp);
-    })
-  }
-  app.listen(app.get('port'));
+app.get("/", function(request, response) {
+  response.render("index");
 });
 
+app.get("/*", function(request, response) {
+  var url = request.params[0];
+  var short_url = "";
+  var json = JSON.stringify({"error":"URL invalid"});
 
+  if(url.match(DET) === null)
+    response.end(json);  
+  
+  shorten(url,function(err,resp){
+    if(err)
+      console.error("Error: "+err);
+    else
+      json = JSON.stringify({"original_url":url,"short_url":resp});
+      
+    response.end(json);            
+  });
+  
+});
+
+app.listen(app.get('port'));
+
+
+function shorten(url, callback)
+{
+    // DB Connection
+    mongo.connect(MONGOLAB_URI, function(err, db) {
+      if(err)
+        console.dir(err);
+      else    
+        console.log("Connected to Mongolab DB");
+
+      var col = db.collection('shorten');
+      col.find({
+        url: url
+      }).toArray(function(err, documents) {
+        console.log("finished looking for url...");
+        var resp = "";
+        if(err)
+          callback(err,null);
+        else if(documents.length)
+          resp = documents[0].short;
+        else
+        {
+          //TODO: Create new short url
+        }
+
+        db.close();
+        callback(null,resp);
+      })
+  });
+      
+}
