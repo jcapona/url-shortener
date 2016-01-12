6a25bdabe0716b5dc041f3e98daf6bd9e15a14e1
Jorge Capona <jcapona@gmail.com>
2016-01-12 14:36:58 -0300

    Use regex to detect a valid URL

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+ var DET = /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/;
+
 var express = require('express');
 var app = express();
 
@@ -8,6 +10,18 @@ app.get("/", function(request, response) {
   response.render("index");
 });
 
+app.get("/*", function(request, response) {
+  var url = request.params[0];
+  var short_url = "";
+  var json = JSON.stringify({"error":"URL invalid"});
+
+  if(url.match(DET) === null)
+    response.end(json);  
+  
+
+  var json = JSON.stringify({"original_url":url,"short_url":short_url});
+  response.end(json);
+});
 
 app.listen(app.get('port'));
 
